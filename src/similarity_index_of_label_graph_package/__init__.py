61620f3766170271d7ae612cf99f431d8abfd55f
fsssosei <43291778+fsssosei@users.noreply.github.com>
2020-04-01 19:37:56 +0800

    Create __init__.py

---
+++ src/similarity_index_of_label_graph_package/__init__.py
@@ -0,0 +1,19 @@
+'''
+similarity_index_of_label_graph - This is the package used to calculate the similarity index of the label graph pairs.
+Copyright (C) 2020  sosei
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from similarity_index_of_label_graph_package.similarity_index_of_label_graph_module import *
